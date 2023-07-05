029f83cde5bb446cd9225773b3e754860e75a8ec
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-05 22:38:57 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home-main-text" id="home">
+      <p className="text-box">
+        I am a new programmer excited to learn and grow in the field of software
+        development. I am constantly learning and working to improve my skills
+        as a developer.
+      </p>
+    </div>
+  );
+}
+
