dad2d4c211f5fad69b9bad100f53ab22c0f61ded
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-05 22:40:19 +0530

    Create Projects.js

---
+++ src/pages/Projects.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="main-projects" id="projects">
+      This is projects section! This is projects section! This is projects
+      section! This is projects section! This is projects section! This is
+      projects section! This is projects section! This is projects section! This
+      is projects section! This is projects section! This is projects section!
+      This is projects section! This is projects section! This is projects
+      section! This is projects section! This is projects section! This is
+      projects section! This is projects section! This is projects section! This
+      is projects section! This is projects section! This is projects section!
+      This is projects section! This is projects section! This is projects
+      section! This is projects section! This is projects section! This is
+      projects section! This is projects section! This is projects section! This
+      is projects section! This is projects section! This is projects section!
+      This is projects section!
+    </div>
+  );
+}
