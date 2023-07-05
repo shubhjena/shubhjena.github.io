96b7cf8107dd99c1d54ac1791f6d0a33b48bd3f9
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-05 22:39:02 +0530

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='main-about' id='about'>
+      This is about section This is about section This is about section This is about section This is about section This is about section This is about section This is about section This is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about section
+    </div>
+  )
+}
