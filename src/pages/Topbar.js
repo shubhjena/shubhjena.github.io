ec401473bf851d14f27149f266545c4e32edd768
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-05 22:38:28 +0530

    Create Topbar.js

---
+++ src/pages/Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Topbar() {
+  return (
+    <div className='top-bar'>
+      <span className='first-name'>
+        Shubhashish
+      </span>
+      <span className='last-name'>
+        Jena
+      </span>
+
+    </div>
+  )
+}
