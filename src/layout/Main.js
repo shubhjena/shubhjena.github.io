a5815f69d84eca2998dfc94ab0eb9f83a8b6a82a
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-08 23:41:56 +0530

    Update Main.js

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,31 +1,38 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from "react";
 import About from "../pages/About";
 import Navbar from "../pages/Navbar";
 import Hero from "./Hero";
 
-const scrollContainerRef = useRef(null);
-
-useEffect(() => {
-  const scrollContainer = scrollContainerRef.current;
-
-  let scrollSpeed = 5000; // Adjust the scroll speed here
-
-  const handleScroll = (event) => {
-    const scrollAmount = event.deltaY > 0 ? scrollSpeed : -scrollSpeed;
-    scrollContainer.scrollLeft += scrollAmount;
-  };
-
-  scrollContainer.addEventListener('wheel', handleScroll);
-
-  return () => {
-    scrollContainer.removeEventListener('wheel', handleScroll);
-  };
-}, []);
-
 export default function Main() {
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+
+    let scrollSpeed = 75; // Adjust the scroll speed here
+
+    const handleScroll = (event) => {
+      const scrollAmount = event.deltaY > 0 ? scrollSpeed : -scrollSpeed;
+      scrollContainer.scrollLeft += scrollAmount;
+    };
+
+    scrollContainer.addEventListener("wheel", handleScroll);
+
+    return () => {
+      scrollContainer.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
+
+  const sections = [
+    { id: "home", label: "Home" },
+    { id: "projects", label: "Projects" },
+    { id: "about", label: "About" },
+    // Add more sections as needed
+  ];
+
   return (
     <div className="App flex">
-      <Navbar />
+      <Navbar sections={sections} />
       <div
         className="main-layout overflow-x-scroll scrollbar-none"
         ref={scrollContainerRef}
