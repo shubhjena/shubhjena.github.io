38829f9ac4bdc1a0fd24b1da6b06fb0f56620638
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-11 02:14:18 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import profilePic from "./Profile picture.jpeg";
 export default function About() {
   return (
-    <div className='main-about' id='about'>
-      This is about section This is about section This is about section This is about section This is about section This is about section This is about section This is about section This is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about sectionThis is about section
+    <div className="flex-col min-w-full bg-zinc-600 z-10" id="about">
+      <div className=" last-name-info text-left ">Jena  </div>
+      <div className="flex pt-8">
+        <img
+          src={profilePic}
+          className="profile-pic rounded-full ml-8 mr-8"
+          alt="profile picture"
+        />
+        <div>
+          This is about section This is about sectionThis is about section This
+          is about section This is about section This is about section This is
+          about section This is about section This is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about
+          sectionThis is about sectionThis is about sectionThis is about section
+        </div>
+      </div>
     </div>
-  )
+  );
 }
