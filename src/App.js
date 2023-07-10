ed99b94fb8762f9978f00a031f1dc2fae25308e1
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-11 02:11:44 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
+import Main from "./layout/Main";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Main/>
+      <Route path="#home"><Home /></Route>
+      <Route path="#projects"><Projects /></Route>
+      <Route path="#about"><About /></Route>
+      <Route path="*" element={<Notfound />} />
+    </BrowserRouter>
+    
   );
 }
 
