b18872a4b9f3b9be06dc43cc6bd0b8013ba1fdb2
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-11 02:11:28 +0530

    Update Topbar.js

--- src/pages/Topbar.js
+++ src/pages/Topbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Topbar() {
   return (
-    <div className='top-bar'>
-      <span className='first-name'>
-        Shubhashish
-      </span>
-      <span className='last-name'>
-        Jena
-      </span>
-
-    </div>
-  )
+    <>
+      <div className=" bg-slate-950 w-screen h-48 fixed"></div>
+      <div className="top-bar fixed z-20">
+        <span className="first-name ">Shubhashish</span>
+      </div>
+      <div className="top-bar fixed">
+        <span className="last-name ml-96">Jena</span>
+      </div>
+    </>
+  );
 }
