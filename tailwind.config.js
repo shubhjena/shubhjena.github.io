418a2a9d2962b67759fe5fde9a6e87379f7b71e7
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-14 00:01:16 +0530

    add custom colours to tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}", "./public/index.html"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#F3F3F3',
+      'black': '#2D3032',
+      'primary': '#F4EEFF',
+      'secondary': '#DCD6F7',
+      'tertiary': '#A6B1E1',
+      'accent': '#424874',
+    },
   },
   plugins: [require("tailwindcss-no-scrollbar")],
 };
