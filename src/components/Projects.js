50ae6dc19dedb567859da5867cb4ce49a376eb73
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-31 04:48:35 +0530

    chore: update projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,11 +5,11 @@ export default function Projects() {
 
   // array to store the project information
   const projects = [
-    {
-      title: "Classic Snake Game",
-      desc: "The classic snake game presented in JAVA GUI",
-      link: "https://github.com/shubhjena/SnakeGame",
-    },
+    // {
+    //   title: "Classic Snake Game",
+    //   desc: "The classic snake game presented in JAVA GUI",
+    //   link: "https://github.com/shubhjena/SnakeGame",
+    // },
     {
       title: "TaskMeowster",
       desc: "A cat themed task listing app to track your pending tasks.",
@@ -24,6 +24,21 @@ export default function Projects() {
       title: "Portfolio Website",
       desc: "My professional portfolio website",
       link: "",
+    },
+    {
+      title: "Quick Cart",
+      desc: "An online shopping mart",
+      link: "https://quick-cart-kohl.vercel.app/",
+    },
+    {
+      title: "My Dental Clinic",
+      desc: "Website for a dental clinic",
+      link: "https://my-dental-clinic.vercel.app/",
+    },
+    {
+      title: "Expense Tracker",
+      desc: "An expense tracker web-app to store and display transactions",
+      link: "https://github.com/shubhjena/ExpenseTracker",
     },
 
     // Add more sections as needed
