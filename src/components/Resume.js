1c895caa0d36ed009f7c9678f415540cd79fbc20
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-11-03 21:20:18 +0530

    refactor: clean up resume code

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,72 +2,28 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import { RESUME_PROJECTS } from "../lib/data";
 
 const styles = {
   body: {
-    margin: 0,
-    height: "100vh",
-    background: "linear-gradient(45deg, #e0a4e5, #84fab0, #a0a1f1, #e0a4e5)",
-    // backgroundColor: "#A9A9A9",
-    overflow: "hidden",
+    background: "linear-gradient(45deg, #e0a4e5, #7FE9DE, #a0a1f1, #e0a4e5)",
+    // background: "#A9A9A9",
   },
   pdfPage: {
     width: "210mm" /* A4 width */,
     height: "297mm" /* A4 height */,
-    margin: "0px auto",
-    backgroundColor: "#fff",
-    // border: '1px solid #030303',
-    padding: "44px",
+    backgroundColor: "#f6f6f6",
   },
 };
 
-const projects = [
-  {
-    title: "Portfolio Website",
-    technologies: ["React", "Tailwind CSS", "Zustand", "GitHub Pages"],
-    urlText: "/shubhjena.github.io",
-    url: "https://shubhjena.github.io/",
-    problemStatement:
-      "Develop a portfolio website to showcase my skills and projects.",
-    description:
-      "Developed a portfolio website with home, projects, and about sections.#The website showcases my skills, projects, and contact information.",
-  },
-  {
-    title: "Expense Tracker",
-    technologies: ["React", "Express.js", "Node.js", "MongoDB", "GitHub"],
-    urlText: "/github/expense-tracker",
-    url: "https://github.com/shubhjena/ExpenseTracker",
-    problemStatement:
-      "Build a web application for recording and tracking expenses.",
-    description:
-      "Created an 'Expense Tracker' web app with React.js and Express.js. #Users can add, view, edit, and delete their expense records. It can be used for expense monitoring. #Data is stored and fetched from a MongoDB database using Mongoose.",
-  },
-  {
-    title: "Quiz Application",
-    technologies: ["Spring Boot", "Java", "Maven", "PostgreSQL", "REST API"],
-    urlText: "/github/Quizapp",
-    url: "https://github.com/shubhjena/Quizapp",
-    problemStatement: "Develop a quiz app with RESTful API endpoints.",
-    description:
-      "Created RESTful API endpoints for quiz creation, submission, and management. #Quiz questions are maintained in a PostgreSQL database to which questions can be added or deleted. #Users can generate a quiz with a requested number of questions in any of the given topics.",
-  },
-  {
-    title: "My Dental Clinic",
-    technologies: ["React", "Material UI", "Tailwind CSS", "GitHub", "Vercel"],
-    urlText: "/my-dental-clinic.vercel.app",
-    url: "https://my-dental-clinic.vercel.app",
-    problemStatement: "Create a front-end for a dental clinic website.",
-    description:
-      "Developed a website for a dental clinic that provides information about their services. #The website also shows their contact information, patient testimonials, doctor’s profiles, equipment, etc.",
-  },
-];
 export default function Resume() {
   return (
     <div
       style={styles.body}
-      className="min-w-min flex flex-col h-full"
+      className="m-0 flex flex-col h-screen overflow-hidden"
       id="resume"
     >
+      {/* Navigation buttons */}
       <div className="fixed right-6 top-6 w-24 h-12 p-0.5 flex text-white  bg-black bg-opacity-60 rounded-full">
         <NavLink
           className="m-auto bg-accent  bg-opacity-60 rounded-full p-2 pl-3 pr-3 my-auto"
@@ -88,11 +44,13 @@ export default function Resume() {
           </button>
         </a>
       </div>
+      {/* Page Container */}
       <div id="pageContainer" className="pt-6 pb-64 overflow-y-auto">
+        {/* A4 Page */}
         <div
           style={styles.pdfPage}
           id="pdfPage"
-          className="shadow-2xl "
+          className="shadow-2xl mx-auto p-11"
         >
           {/* Heading */}
           <div className=" text-center">
@@ -149,7 +107,7 @@ export default function Resume() {
             <h2 className="text-lg font-semibold">Projects</h2>
             <hr />
             <ul className="list-none  ml-6 mr-2 ">
-              {projects.map((project, index) => (
+              {RESUME_PROJECTS.map((project, index) => (
                 <li key={index} className="mt-2">
                   <h3 className="text-md font-semibold">
                     <span>{project.title} </span>
--- src/lib/data.js
+++ src/lib/data.js
@@ -54,3 +54,45 @@ export const PROJECTS = [
 
     // Add more sections as needed
   ];
+
+  export const RESUME_PROJECTS = [
+    {
+      title: "Portfolio Website",
+      technologies: ["React", "Tailwind CSS", "Zustand", "GitHub Pages"],
+      urlText: "/shubhjena.github.io",
+      url: "https://shubhjena.github.io/",
+      problemStatement:
+        "Develop a portfolio website to showcase my skills and projects.",
+      description:
+        "Developed a portfolio website with home, projects, and about sections.#The website showcases my skills, projects, and contact information.",
+    },
+    {
+      title: "Expense Tracker",
+      technologies: ["React", "Express.js", "Node.js", "MongoDB", "GitHub"],
+      urlText: "/github/expense-tracker",
+      url: "https://github.com/shubhjena/ExpenseTracker",
+      problemStatement:
+        "Build a web application for recording and tracking expenses.",
+      description:
+        "Created an 'Expense Tracker' web app with React.js and Express.js. #Users can add, view, edit, and delete their expense records. It can be used for expense monitoring. #Data is stored and fetched from a MongoDB database using Mongoose.",
+    },
+    {
+      title: "Quiz Application",
+      technologies: ["Spring Boot", "Java", "Maven", "PostgreSQL", "REST API"],
+      urlText: "/github/Quizapp",
+      url: "https://github.com/shubhjena/Quizapp",
+      problemStatement: "Develop a quiz app with RESTful API endpoints.",
+      description:
+        "Created RESTful API endpoints for quiz creation, submission, and management. #Quiz questions are maintained in a PostgreSQL database to which questions can be added or deleted. #Users can generate a quiz with a requested number of questions in any of the given topics.",
+    },
+    {
+      title: "My Dental Clinic",
+      technologies: ["React", "Material UI", "Tailwind CSS", "GitHub", "Vercel"],
+      urlText: "/my-dental-clinic.vercel.app",
+      url: "https://my-dental-clinic.vercel.app",
+      problemStatement: "Create a front-end for a dental clinic website.",
+      description:
+        "Developed a website for a dental clinic that provides information about their services. #The website also shows their contact information, patient testimonials, doctor’s profiles, equipment, etc.",
+    },
+  ];
+  
