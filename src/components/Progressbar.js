016dbb75b67d6981c93df14b430a2ed45efedb04
shubhjena <117462852+shubhjena@users.noreply.github.com>
2024-02-28 04:08:11 +0530

    perf: throttle progress bar

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -4,13 +4,44 @@ import { countStore } from "../store/ScrollStatus";
 function Progressbar() {
   const { scrollProgress, updateScrollProgress } = countStore();
 
-  // scroll event listener -wheel
-  useEffect(() => {
-    const interval = setInterval(updateScrollProgress, 30);
-    return () => {
-      clearInterval(interval);
+   // Custom throttle function
+   const throttle = (func, delay=60) => {
+    let throttled = false;
+    let lastArgs = null;
+
+    return (...args) => {
+      if (!throttled) {
+        throttled = true;
+        func(...args);
+        setTimeout(() => {
+          throttled = false;
+          if (lastArgs) {
+            func(...lastArgs);
+            lastArgs = null;
+          }
+        }, delay);
+      }else{
+        lastArgs = args;
+      }
     };
-  }, [scrollProgress,updateScrollProgress]);
+  };
+
+  // Throttle the scroll event
+  const throttledScrollHandler = throttle(updateScrollProgress, 50);
+
+  const handleScroll = () => {
+    throttledScrollHandler();
+  };
+
+  useEffect(() => {
+    // Add wheel event listener
+    window.addEventListener('wheel', handleScroll);
+
+    // Cleanup: Remove wheel event listener on component unmount
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  },[throttledScrollHandler]);
 
   return (
     <div className="fixed bottom-0 md:bottom-8 right-0 md:right-8 w-full md:w-52 h-1.5 md:h-1 bg-transparent md:border border-accent">
